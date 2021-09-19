821408b2223b16865fa0e9534afedb49afbcbeab
Chepkorir Brenda <40006302+peta-byte@users.noreply.github.com>
2021-09-19 10:48:11 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+import HomeScreen from './src/HomeScreen';
+import DetailsScreen from './src/DetailsScreen';
 
 
 const Stack = createStackNavigator();
