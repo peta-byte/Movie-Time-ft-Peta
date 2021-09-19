e591a5ea67268d06db1841a8e15da73c9702ada5
Chepkorir Brenda <40006302+peta-byte@users.noreply.github.com>
2021-09-19 10:40:09 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="App_to_Home" screenOptions={
+        { headerTintColor: 'white', headerStyle: {backgroundColor: 'orange'}}
+      }>
+        <Stack.Screen component={HomeScreen} name="App_to_Home" options={{title: 'Movie Time'}} />
+        <Stack.Screen component={DetailsScreen} name="Home_to_Details" options={({route}) => ({title: route.params.movie.title})} />
+        <Stack.Screen component={DetailsScreen} name="Details_to_Details" options={({route}) => ({title: route.params.movie.title})} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
